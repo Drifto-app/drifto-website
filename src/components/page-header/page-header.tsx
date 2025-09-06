33698a7aa5722032d21df6011ef2ee111080b96e
reuben <reubenalabi2006@gmail.com>
2025-09-06 14:40:06 +0100

    fix: Made some styling and logic fixes

--- src/components/page-header/page-header.tsx
+++ src/components/page-header/page-header.tsx
@@ -1,18 +1,21 @@
 import { Settings } from "lucide-react";
 import React from "react";
+import {useRouter} from "next/navigation";
 
 interface PageHeaderProp {
-  headerTitle: String;
+  headerTitle: string;
 }
 
 export default function PageHeader({ headerTitle }: PageHeaderProp) {
+    const router = useRouter();
+
   return (
     <div>
-      <div className=" flex justify-between font-bold">
-        <h2 className=" text-4xl">{headerTitle}</h2>
-        <a href="setting">
+      <div className="w-full flex justify-between font-bold px-4 items-center">
+        <h2 className=" text-3xl">{headerTitle}</h2>
+        <span onClick={() => router.push(`/m/settings?prev=${encodeURIComponent("/?screen=plans")}`)}>
           <Settings size={"25px"} />
-        </a>
+        </span>
       </div>
     </div>
   );

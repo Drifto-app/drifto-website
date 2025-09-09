d99c736ae38ec6ab1ee1cb3081b5fe4006dae238
reuben <reubenalabi2006@gmail.com>
2025-09-10 00:57:05 +0100

    feat: removed split code for paystack

--- src/components/payment/pay-button.tsx
+++ src/components/payment/pay-button.tsx
@@ -32,7 +32,6 @@ export const PayButton = ({
             email,
             channels: [paymentType ?? "card"],
             reference,
-            // split_code: process.env.NEXT_PUBLIC_PAYSTACK_SPLITCODE!,
             onSuccess: () => {
                 if (onSuccess) onSuccess();
             },
